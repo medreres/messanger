d6827c8dd0185a7b9067948f4e3bbf44b4f69e48
medreres <90519417+medreres@users.noreply.github.com>
2022-12-13 20:41:36 +0200

    added server url

--- src/contexts/SocketProvider.jsx
+++ src/contexts/SocketProvider.jsx
@@ -11,7 +11,7 @@ const SocketProvider = ({ children, id }) => {
   const [socket, setSocket] = useState();
 
   useEffect(() => {
-    const newSocket = io("192.168.31.11:5001", {
+    const newSocket = io("https://messanger-react.herokuapp.com/", {
       withCredentials: true,
       query: {
         id,
