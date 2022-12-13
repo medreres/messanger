4342b1cdef871ba814271433aa91775f52389ad4
medreres <90519417+medreres@users.noreply.github.com>
2022-12-13 20:31:13 +0200

    testing server

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    res.send('hello world');
+
+})
+
+app.listen(port, () => {
+    console.log(`Started running server on port ${port}`);
+});
 const io = require('socket.io')(5001, {
     cors: {
         origin: "192.168.31.11:3000/",
         methods: ["GET", "POST"],
         allowedHeaders: ["my-custom-header"],
         credentials: true
-      }
+    }
 })
 
 io.on("error", (err) => {
     console.log(`connect_error due to ${err.message}`);
-  });
+});
 
 io.on('connection', socket => {
     const id = socket.handshake.query.id;
