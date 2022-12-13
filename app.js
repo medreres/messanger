1c15f88312dbe0fff7286e2c41b903df9373eb12
medreres <90519417+medreres@users.noreply.github.com>
2022-12-13 20:36:16 +0200

    socket

--- app.js
+++ app.js
@@ -1,46 +1,38 @@
 const express = require('express');
-const app = express();
 const port = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-    res.send('hello world');
 
+const io = require('socket.io')(port, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+    }
 })
 
-app.listen(port, () => {
-    console.log(`Started running server on port ${port}`);
+io.on("error", (err) => {
+    console.log(`connect_error due to ${err.message}`);
 });
-// const io = require('socket.io')(port, {
-//     cors: {
-//         origin: "192.168.31.11:3000/",
-//         methods: ["GET", "POST"],
-//         allowedHeaders: ["my-custom-header"],
-//         credentials: true
-//     }
-// })
 
-// io.on("error", (err) => {
-//     console.log(`connect_error due to ${err.message}`);
-// });
+io.on('connection', socket => {
+    const id = socket.handshake.query.id;
 
-// io.on('connection', socket => {
-//     const id = socket.handshake.query.id;
+    console.log(id)
 
-//     console.log(id)
+    socket.join(id);
 
-//     socket.join(id);
-
-//     socket.on('send-message', ({
-//         recipients,
-//         text
-//     }) => {
-//         recipients.forEach(recipient => {
-//             const newRecipients = recipients.filter(r => r !== recipient)
-//             newRecipients.push(id);
-//             socket.broadcast.to(recipient).emit('receive-message', {
-//                 recipients: newRecipients,
-//                 sender: id,
-//                 text
-//             })
-//         })
-//     })
-// })
+    socket.on('send-message', ({
+        recipients,
+        text
+    }) => {
+        recipients.forEach(recipient => {
+            const newRecipients = recipients.filter(r => r !== recipient)
+            newRecipients.push(id);
+            socket.broadcast.to(recipient).emit('receive-message', {
+                recipients: newRecipients,
+                sender: id,
+                text
+            })
+        })
+    })
+})
