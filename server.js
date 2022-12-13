2a1750f1c337f52bdaf5f21630eb492aaab6dc62
medreres <90519417+medreres@users.noreply.github.com>
2022-12-13 20:12:25 +0200

    hello world

--- server.js
+++ server.js
@@ -1,35 +1,8 @@
-const io = require('socket.io')(5001, {
-    cors: {
-        origin: "192.168.31.11:3000/",
-        methods: ["GET", "POST"],
-        allowedHeaders: ["my-custom-header"],
-        credentials: true
-      }
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 })
-
-io.on("error", (err) => {
-    console.log(`connect_error due to ${err.message}`);
-  });
-
-io.on('connection', socket => {
-    const id = socket.handshake.query.id;
-
-    console.log(id)
-
-    socket.join(id);
-
-    socket.on('send-message', ({
-        recipients,
-        text
-    }) => {
-        recipients.forEach(recipient => {
-            const newRecipients = recipients.filter(r => r !== recipient)
-            newRecipients.push(id);
-            socket.broadcast.to(recipient).emit('receive-message', {
-                recipients: newRecipients,
-                sender: id,
-                text
-            })
-        })
-    })
-})
+const port = process.env.PORT || 3000;
+app.listen(port)
