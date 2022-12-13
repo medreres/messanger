f518ba90e994e8815da0b7a1b5bc78b2b322e5fc
medreres <90519417+medreres@users.noreply.github.com>
2022-12-13 20:34:08 +0200

    testing

--- app.js
+++ app.js
@@ -9,38 +9,38 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Started running server on port ${port}`);
 });
-const io = require('socket.io')(port, {
-    cors: {
-        origin: "192.168.31.11:3000/",
-        methods: ["GET", "POST"],
-        allowedHeaders: ["my-custom-header"],
-        credentials: true
-    }
-})
+// const io = require('socket.io')(port, {
+//     cors: {
+//         origin: "192.168.31.11:3000/",
+//         methods: ["GET", "POST"],
+//         allowedHeaders: ["my-custom-header"],
+//         credentials: true
+//     }
+// })
 
-io.on("error", (err) => {
-    console.log(`connect_error due to ${err.message}`);
-});
+// io.on("error", (err) => {
+//     console.log(`connect_error due to ${err.message}`);
+// });
 
-io.on('connection', socket => {
-    const id = socket.handshake.query.id;
+// io.on('connection', socket => {
+//     const id = socket.handshake.query.id;
 
-    console.log(id)
+//     console.log(id)
 
-    socket.join(id);
+//     socket.join(id);
 
-    socket.on('send-message', ({
-        recipients,
-        text
-    }) => {
-        recipients.forEach(recipient => {
-            const newRecipients = recipients.filter(r => r !== recipient)
-            newRecipients.push(id);
-            socket.broadcast.to(recipient).emit('receive-message', {
-                recipients: newRecipients,
-                sender: id,
-                text
-            })
-        })
-    })
-})
+//     socket.on('send-message', ({
+//         recipients,
+//         text
+//     }) => {
+//         recipients.forEach(recipient => {
+//             const newRecipients = recipients.filter(r => r !== recipient)
+//             newRecipients.push(id);
+//             socket.broadcast.to(recipient).emit('receive-message', {
+//                 recipients: newRecipients,
+//                 sender: id,
+//                 text
+//             })
+//         })
+//     })
+// })
